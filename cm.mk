25d485873e1997accd0c44d900aabbe2d86a6467
h8rift <h8rift@gmail.com>
2012-10-13 23:12:12 -0500

    evita: Update fingerprint to 2.20 RUU
    Change-Id: I0a4b5bae555d71af7f971ea115a691e02e083d58

--- cm.mk
+++ cm.mk
@@ -18,4 +18,4 @@ PRODUCT_MODEL := One X
 PRODUCT_MANUFACTURER := HTC
 
 # Set build fingerprint / ID / Product Name etc.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_evita BUILD_FINGERPRINT=cingular_us/evita/evita:4.0.4/IMM76D/79936.7:user/release-keys PRIVATE_BUILD_DESC="2.20.502.7 CL79936 release-keys" BUILD_NUMBER=79936
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_evita BUILD_FINGERPRINT=cingular_us/evita/evita:4.0.4/IMM76D/79936.7:user/release-keys PRIVATE_BUILD_DESC="2.20.502.7 CL79936 release-keys"
