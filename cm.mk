c9b0e8afefdf7ba741246c68f1d6a744b0869bd2
Shane Passmore <h8rift@gmail.com>
2012-12-06 18:49:27 -0600

    evita: define boot animation name properly.

--- cm.mk
+++ cm.mk
@@ -4,6 +4,8 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 # Release name
 PRODUCT_RELEASE_NAME := evita
 
+TARGET_BOOTANIMATION_NAME := 720
+
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
