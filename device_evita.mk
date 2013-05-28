d02b1c620343b963859d170f27ecd54ceab9522e
Ethan Chen <intervigil@gmail.com>
2013-05-28 15:11:24 -0700

    evita: move GSM permission up the inheritance chain
    Change-Id: I18da4213c0ec9b274d39e84ea2a415ceb345a8cb

--- device_evita.mk
+++ device_evita.mk
@@ -86,7 +86,6 @@ PRODUCT_PACKAGES += \
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
 
 # Extra properties
