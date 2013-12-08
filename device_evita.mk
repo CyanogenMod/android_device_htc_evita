76989e5418f2f8a9886a798804f93e6071066b6a
h8rift <h8rift@gmail.com>
2013-12-08 11:25:47 -0600

    evita: enable NFC host-based card emulation
    Change-Id: I693ee0fb1dac611e0f30c97c4ace150fb00892e2

--- device_evita.mk
+++ device_evita.mk
@@ -87,6 +87,7 @@ PRODUCT_PACKAGES += \
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
 
 # Extra properties
