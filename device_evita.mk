d4486a2ee6a4dcc80c183218c989cfe51d340021
jrior001 <jriordan001@gmail.com>
2014-11-25 21:58:04 -0500

    Evita: disable hce
    Change-Id: I73191330fc75f8bdfa80fbc940b5bc12979a81c9

--- device_evita.mk
+++ device_evita.mk
@@ -80,7 +80,6 @@ PRODUCT_PACKAGES += \
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
 
 # Extra properties
