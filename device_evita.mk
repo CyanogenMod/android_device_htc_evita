f2169586c81acab1e02f7ab9704c35532d9c1b55
JoseGalRe <josegalre@gmail.com>
2015-02-12 12:40:06 -0600

    evita: remove deprecated torch package
    Change-Id: Idbe2b4835a37a17c87296893666ff4d9e4ecce05

--- device_evita.mk
+++ device_evita.mk
@@ -73,10 +73,6 @@ PRODUCT_PACKAGES += \
     Tag \
     com.android.nfc_extras
 
-# Torch
-PRODUCT_PACKAGES += \
-    Torch
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
